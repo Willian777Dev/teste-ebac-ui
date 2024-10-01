4be80758411570026b774d7349ba4db465ea59fe
Wilian Koppe <williankoppe70@gmail.com>
2024-10-01 18:51:19 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -20,10 +20,27 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Ariel Roll Sleeve Sweatshirt')
+        cy.get('.product_title').should('contain', 'Ariel Roll Sleeve Sweatshirt')
     });
 
     it('Deve adicionar o produto ao carrinho', () => {
-        
+        let qtd = 3
+        produtosPage.buscarProduto('Zoltan Gym Tee')
+        produtosPage.addProdutoCarrinho('XS', 'Green', qtd)
+
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Zoltan Gym Tee” foram adicionados no seu carrinho.')
     });
+
+    it.only('Deve adicionar o produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade)
+
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+    });
+
+        })
+
+        
 });
