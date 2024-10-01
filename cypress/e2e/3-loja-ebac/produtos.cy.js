e1409058bf1fd2dd6c412e0fd5b3292cdf1e24eb
Wilian Koppe <williankoppe70@gmail.com>
2024-10-01 17:23:09 -0300

    Introduzindo Page Objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,29 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista', () => {
-        cy.get(' .product-block')
-                //.first()
-                //.last()
-                //.contains('Abominable Hoodie')
-                .eq(5)
-                .click()
+        produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
         
-                cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Ariel Roll Sleeve Sweatshirt'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar o produto ao carrinho', () => {
         
     });
 });
